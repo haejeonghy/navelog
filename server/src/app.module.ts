d77547abf005bedd16e143d76d7d6a13acce3d3b
jihye_Park <sconenomad@gmail.com>
2022-10-14 14:35:46 +0900

    Feat:hashtag추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HashtagsController } from './hashtags/hashtags.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, HashtagsController],
   providers: [AppService],
 })
 export class AppModule {}
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
+//function을 놓는 곳! 비지니스 로직 실행
 @Injectable()
 export class AppService {
   getHello(): string {
