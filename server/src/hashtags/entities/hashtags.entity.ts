b064684e9c01cc18f4591e9b04011dd600ba32d8
jihye_Park <sconenomad@gmail.com>
2022-10-28 13:55:42 +0900

    Feat:entities변경

--- server/src/hashtags/entities/hashtags.entity.ts
+++ server/src/hashtags/entities/hashtags.entity.ts
@@ -1,4 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+
+@Entity('hashtags')
 export class Hashtag {
+  @PrimaryGeneratedColumn()
+  @ApiProperty({ description: 'primary key Id' })
   id: number;
-  name: string;
+
+  @Column({ type: 'varchar', length: 45, nullable: false })
+  @ApiProperty({ description: '해시태그' })
+  tag: string;
 }
